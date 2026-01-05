909460e1e37fd88a8ec9bc28b7d574ff1fcb0d0c
mika1407 <koneautomaatio@hotmail.com>
2026-01-05 15:50:51 +0200

    background made

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 800, 600
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Dodge")
+
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
+
+def draw():
+    WINDOW.blit(BG, (0, 0))
+    pygame.display.update()
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
