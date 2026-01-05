b5ae5e1b50e02d797e0095ffd2d5fe118e9107ca
mika1407 <koneautomaatio@hotmail.com>
2026-01-05 17:50:27 +0200

    time added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 800, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -10,9 +11,14 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60
 PLAYER_VEL = 5
+FONT = pygame.font.SysFont("comicsans", 30)
 
-def draw(player):
+def draw(player, elapsed_time):
     WINDOW.blit(BG, (0, 0))
+    
+    time_text = FONT.render(f"Time: {int(elapsed_time)}s", 1, ("white"))
+    WINDOW.blit(time_text, (10, 10))
+
     pygame.draw.rect(WINDOW, (255, 0, 0), player)
     pygame.display.update()
 
@@ -21,9 +27,12 @@ def main():
 
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
     clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
 
     while run:
         clock.tick(60)
+        elapsed_time = time.time() - start_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -36,7 +45,7 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        draw(player, elapsed_time)
 
     pygame.quit()
 
