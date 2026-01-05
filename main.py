38bd0a5b5a1e3f004b5858949ee823af9f06a36f
mika1407 <koneautomaatio@hotmail.com>
2026-01-05 17:39:38 +0200

    player keys added

--- main.py
+++ main.py
@@ -8,20 +8,35 @@ pygame.display.set_caption("Space Dodge")
 
 BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60
+PLAYER_VEL = 5
+
+def draw(player):
     WINDOW.blit(BG, (0, 0))
+    pygame.draw.rect(WINDOW, (255, 0, 0), player)
     pygame.display.update()
 
 def main():
     run = True
 
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
+            player.x += PLAYER_VEL
+
+        draw(player)
 
     pygame.quit()
 
